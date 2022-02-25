5944491a8e63983ccc9271c0c462d8731bcc01b2
ozzy-cydeo <ozzy@cybertekschool.com>
2022-02-24 21:18:56 -0500

    Spring Boot Demo

--- Spring-06-SpringBoot-Demo/src/main/java/com/cydeo/service/CommentService.java
+++ Spring-06-SpringBoot-Demo/src/main/java/com/cydeo/service/CommentService.java
@@ -4,10 +4,12 @@ import com.cydeo.model.Comment;
 import com.cydeo.proxy.CommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CommentService {
+
 
     private final CommentRepository commentRepository;
     private final CommentNotificationProxy commentNotificationProxy;
