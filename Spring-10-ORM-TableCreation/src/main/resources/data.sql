613325b997182e2f6093f31ac2452737940f78c9
ozzy-cydeo <ozzy@cybertekschool.com>
2022-04-14 19:42:44 -0400

    ORM Part-1

--- Spring-10-ORM-TableCreation/src/main/resources/data.sql
+++ Spring-10-ORM-TableCreation/src/main/resources/data.sql
@@ -3,7 +3,7 @@
 -- VALUES(1,'Mike Smith');
 
 
-INSERT INTO student(id,first_name,last_name,email)
-VALUES(1,'Mike',' Smith','mike@cydeo.com');
+INSERT INTO student(first_name,last_name,email)
+VALUES('Mike',' Smith','mike@cydeo.com');
 
 
