429be623a2b9d49860bc54fd11dfafd3a0ecfe90
ozzy-cydeo <ozzy@cybertekschool.com>
2022-04-14 20:32:11 -0400

    ORM Part-2

--- Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/DebitAccount.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/DebitAccount.java
@@ -1,7 +1,10 @@
 package com.cydeo.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.math.BigDecimal;
 
+@Entity
 public class DebitAccount extends Account {
 
     private BigDecimal overDraftFee;
