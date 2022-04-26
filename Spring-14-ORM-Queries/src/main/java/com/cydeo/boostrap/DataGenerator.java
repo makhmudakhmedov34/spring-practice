90a5769104b925eff1120820824b8f6a21ec6c24
ozzy-cydeo <ozzy@cybertekschool.com>
2022-04-26 19:58:03 -0400

    Queries

--- Spring-14-ORM-Queries/src/main/java/com/cydeo/boostrap/DataGenerator.java
+++ Spring-14-ORM-Queries/src/main/java/com/cydeo/boostrap/DataGenerator.java
@@ -1,5 +1,6 @@
 package com.cydeo.boostrap;
 
+import com.cydeo.repository.DepartmentRepository;
 import com.cydeo.repository.RegionRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -8,9 +9,11 @@ import org.springframework.stereotype.Component;
 public class DataGenerator implements CommandLineRunner {
 
     private final RegionRepository regionRepository;
+    private final DepartmentRepository departmentRepository;
 
-    public DataGenerator(RegionRepository regionRepository) {
+    public DataGenerator(RegionRepository regionRepository, DepartmentRepository departmentRepository) {
         this.regionRepository = regionRepository;
+        this.departmentRepository = departmentRepository;
     }
 
     @Override
@@ -25,6 +28,16 @@ public class DataGenerator implements CommandLineRunner {
         System.out.println("findTopByCountry:" + regionRepository.findTop2ByCountry("Canada"));
 
         System.out.println("-----------------REGION END-----------------");
+
+        System.out.println("-----------------DEPARTMENT START-----------------");
+
+        System.out.println("findByDepartment:" + departmentRepository.findByDepartment("Toys"));
+        System.out.println("findByDivisionIs:" + departmentRepository.findByDivisionIs("Outdoors"));
+        System.out.println("findDistinctTop3ByDivisionContains:" + departmentRepository.findDistinctTop3ByDivisionContains("Hea"));
+
+
+
+        System.out.println("-----------------DEPARTMENT END-----------------");
 
     }
 }
