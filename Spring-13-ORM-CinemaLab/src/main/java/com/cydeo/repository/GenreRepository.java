f0392fc7ac97bc8abada70b59155f5c19ff76407
CundullahT-CT <cterzioglu@cybertekschool.com>
2022-04-29 02:02:40 +0200

    CinemaLab Repos

--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/GenreRepository.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/GenreRepository.java
@@ -1,2 +1,19 @@
-package com.cydeo.repository;public interface GenreRepository {
+package com.cydeo.repository;
+
+import com.cydeo.entity.Account;
+import com.cydeo.entity.Cinema;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Cinema, Long> {
+
+    // ------------------- JPQL QUERIES ------------------- //
+
+    //Write a JPQL query that return all genres
+
+    // ------------------- Native QUERIES ------------------- //
+
+    //Write a native query that returns genres by containing name
+
 }
