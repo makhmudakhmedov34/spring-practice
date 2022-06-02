315b5a99e2df77822044ac03bdd90fcf312dea94
ozzy-cydeo <ozzy@cybertekschool.com>
2022-06-02 18:13:05 -0400

    Commit

--- Spring-15-REST/src/main/java/com/cydeo/dto/CourseDTO.java
+++ Spring-15-REST/src/main/java/com/cydeo/dto/CourseDTO.java
@@ -1,29 +1,30 @@
 package com.cydeo.dto;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
-@Data
-@Entity
-@Table(name = "COURSES")
+@NoArgsConstructor
+@Getter
+@Setter
 public class CourseDTO {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ID")
+
     private Long id;
 
-    @Column(name = "NAME")
+
     private String name;
 
-    @Column(name = "CATEGORY")
+
     private String category;
 
-    @Column(name = "RATING")
+
     private int rating;
 
-    @Column(name = "DESCRIPTION")
+
     private String description;
 
 }
