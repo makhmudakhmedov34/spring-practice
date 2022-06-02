4e59c322238a153c74ba5d11e973e7408c802cab
ozzy-cydeo <ozzy@cybertekschool.com>
2022-06-02 19:18:38 -0400

    Rest

--- Spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
+++ Spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
@@ -3,15 +3,11 @@ package com.cydeo.controller;
 import com.cydeo.dto.CourseDTO;
 import com.cydeo.service.CourseService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
-@RestController
+@RestController // @Controller + @ResponseBody
 @RequestMapping("/courses")
 public class CourseController {
 
@@ -27,6 +23,12 @@ public class CourseController {
         return list;
 
     }
+
+    @GetMapping("{id}")
+    public CourseDTO getCourseById(@PathVariable("id") Long courseId){
+        return courseService.getCourseById(courseId);
+    }
+
 
 
 }
