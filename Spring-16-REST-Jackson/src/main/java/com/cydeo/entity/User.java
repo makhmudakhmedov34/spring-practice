811333a50de4ff489a0935bfc9903c409cebea27
ozzy-cydeo <ozzy@cybertekschool.com>
2022-06-02 21:08:40 -0400

    Rest

--- Spring-16-REST-Jackson/src/main/java/com/cydeo/entity/User.java
+++ Spring-16-REST-Jackson/src/main/java/com/cydeo/entity/User.java
@@ -1,7 +1,9 @@
 package com.cydeo.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,12 +13,17 @@ import javax.persistence.*;
 @Data
 @NoArgsConstructor
 @Table(name = "user_account")
+@JsonIgnoreProperties(value = "{hibernateLazyInitializer}",ignoreUnknown = true)
 public class User extends BaseEntity {
 
 //    @JsonIgnore
     private String email;
 
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
+
+
     private String username;
 
     @OneToOne(fetch = FetchType.LAZY)
