f4e9bb7a857614f36645ee257c14d8af695e0c1c
makhmudakhmedov34 <akhmedov_makhmud@yahoo.com>
2023-02-12 14:28:03 -0500

    Updates

--- Spring-10-ORM-TableCreation/src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-
--- INSERT INTO EMPLOYEES(id,name)
--- VALUES(1,'Mike Smith');
-
-
-INSERT INTO student(first_name,last_name,email)
-VALUES('Mike',' Smith','mike@cydeo.com');
-
-
--- Spring-23-UnitTesting-MockMvc/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO student(first_name, last_name, age)
-VALUES ('Mike', 'Smith', 20);
