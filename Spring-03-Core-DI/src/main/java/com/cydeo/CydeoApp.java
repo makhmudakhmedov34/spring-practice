b8da3200fa75c85958b988d39fb3d4b160a711a4
makhmudakhmedov34 <akhmedov_makhmud@yahoo.com>
2023-02-13 00:03:27 -0500

    Dependency Injection

--- Spring-03-Core-DI/src/main/java/com/cydeo/CydeoApp.java
+++ Spring-03-Core-DI/src/main/java/com/cydeo/CydeoApp.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class CydeoApp {
     public static void main(String[] args) {
 
-
         ApplicationContext context = new AnnotationConfigApplicationContext(ConfigApp.class);
         Java java = context.getBean(Java.class);
         java.getTeachingHours();
