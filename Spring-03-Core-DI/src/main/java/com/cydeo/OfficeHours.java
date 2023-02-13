b3407f05993d1b1a801cc6dda790ee2ff4c04b51
makhmudakhmedov34 <akhmedov_makhmud@yahoo.com>
2023-02-12 23:42:34 -0500

    Dependency Injection

---
+++ Spring-03-Core-DI/src/main/java/com/cydeo/OfficeHours.java
@@ -0,0 +1,12 @@
+package com.cydeo;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OfficeHours {
+
+    public int getHours(){
+        return 5;
+    }
+}
