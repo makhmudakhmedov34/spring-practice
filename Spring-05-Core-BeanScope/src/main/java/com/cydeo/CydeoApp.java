517bb0d5fb4bde6acdbdfba7270815ea338259ff
makhmudakhmedov34 <akhmedov_makhmud@yahoo.com>
2023-02-13 23:42:04 -0500

    Bean Scope

--- Spring-05-Core-BeanScope/src/main/java/com/cydeo/CydeoApp.java
+++ Spring-05-Core-BeanScope/src/main/java/com/cydeo/CydeoApp.java
@@ -1,7 +1,26 @@
 package com.cydeo;
 
+import com.cydeo.config.ProjectConfig;
+import com.cydeo.model.Comment;
+import com.cydeo.service.CommentService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
 public class CydeoApp {
     public static void main(String[] args) {
+        Comment comment = new Comment();
+        comment.setAuthor("Johnson");
+        comment.setText("Spring Framework");
 
+        ApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        CommentService cS1 = context.getBean(CommentService.class);
+        CommentService cS2 = context.getBean(CommentService.class);
+
+        System.out.println(cS1);
+        System.out.println(cS2);
+        System.out.println(cS1==cS2);
     }
 }
