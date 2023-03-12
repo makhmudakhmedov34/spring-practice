5f83933ac7d4ca36bace5b8ed73fcab5fe10337e
makhmudakhmedov34 <akhmedov_makhmud@yahoo.com>
2023-03-11 21:42:38 -0500

    Spring Thymeleaf

---
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/model/Mentor.java
@@ -0,0 +1,19 @@
+package com.cydeo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Mentor {
+
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String gender;
+    private String batch;
+    private String graduated;
+    private String company;
+}
